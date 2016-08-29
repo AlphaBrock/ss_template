32bbd68bcde630e2efb290a6b361d01290d73bd1
AlphaBrock <528327016@qq.com>
2016-08-29 19:26:39 +0800

    tos

---
+++ tos.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+   <title>Freedom</title>
+   
+  <meta name="description" content="shadowsocks, shadowsocksR, VPN, GFW, Freedom, youtube, facebook, twitter, surge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> 
+  <link rel="stylesheet" href="theme/freedom/css/bootstrap.css" type="text/css">
+  <link rel="stylesheet" href="theme/freedom/css/animate.css" type="text/css">
+  <link rel="stylesheet" href="theme/freedom/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="theme/freedom/css/font.css" type="text/css">
+  <link rel="stylesheet" href="theme/freedom/css/landing.css" type="text/css">
+  <link rel="stylesheet" href="theme/freedom/css/app.css" type="text/css">
+  <link rel="shortcut icon" href="theme/freedom/images/favicon.ico">
+  
+  <!--[if lt IE 9]>
+    <script src="js/ie/html5shiv.js"></script>
+    <script src="js/ie/respond.min.js"></script>
+    <script src="js/ie/excanvas.js"></script>
+  <![endif]-->
+</head>
+<body>
+  	
+  <!-- header -->
+  <header id="header" class="navbar navbar-fixed-top bg-white box-shadow b-b b-light affix-top" data-spy="affix" data-offset-top="1">
+    <div class="container">
+      <div class="navbar-header">        
+        <a href="index.php" class="navbar-brand"><img src="theme/freedom/images/logo.png" class="m-r-sm"><span class="text-muted">Freedom</span></a>
+        <button class="btn btn-link visible-xs" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+          <i class="fa fa-bars"></i>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active">
+            <a href="index.php">网站首页</a>
+          </li>
+          <li>
+            <a href="services.php">客户端下载</a>
+          </li>		  
+          <li>
+            <a href="help.php">使用教程</a>
+          </li>
+          <li><a href="tos.php">服务条款</a>
+          </li>
+          <li>
+          	<a href="code.php">邀请码</a>
+          </li>
+		    <li>
+            <div class="m-t-sm"> <a href="auth/login.php" class="btn btn-warning btn-sm m-l">会员登录</a><a href="auth/register.php" class="btn btn-sm btn-success m-l"><strong>用户注册</strong></a>  </div>
+          </li>
+		   <li>
+		      <br>
+		   </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+         
+  <!-- / header -->
+
+<section id="content">
+
+
+<div class="container m-t-lg m-b-lg">
+			<div class="row">
+        <div class="col-sm-12 text-center">
+<div class="blog-post">		              	
+            <div class="post-item">
+            	<div class="post-media">
+            		<section class="panel bg-primary dk m-b-none">
+                  <div class="carousel auto slide panel-body" id="c-fade">      
+                        <div class="item active text-center">
+                     <span class="h1">用户协议 Terms of Service</span>                       
+                      </div>                    
+                  </div> 
+                </section>
+            	</div>
+				
+				
+<h2 class="post-title">隐私.</h2>
+			   <div class="post-sum"><p>    
+            用户注册时候需要提供邮箱以及密码，并自行保管。<br>邮箱为用户的唯一凭证。<br>
+本站会加密存储用户密码，尽量保证数据安全，但并不保证这些信息的绝对安全。 </p>			 
+			   <div class="line line-lg"></div>   
+  <h2 class="post-title">使用条款.</h2>
+  <div class="post-sum"><p>禁止使用本站服务进行任何违法恶意活动。<br>
+使用任何节点，需遵循节点所属国家的相关法律以及中国法律。<br>我们不承担因使用本站服务所发生的一切事故纠纷。<br>
+禁止滥用本站提供的服务。<br>
+对于潜水一个月的用户，我们有权在不通知的情况下删除账户。<br>
+任何违法使用条款的用户，我们将会删除违规账户并没收使用本站服务的权利。</p>
+  <div class="line line-lg"></div>  		 
+          <h2 class="post-title">其它.</h2>
+		<div class="post-sum"><p>本站仅限人类及猫注册使用。<br>
+TOS更新时用户需要遵守最新TOS。</p><br>	
+		
+		 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+	</div></div></section>
+<footer id="footer">
+  	<div class="bg-primary text-center">
+      <div class="container wrapper">
+        <div class="m-t-xl m-b">
+          马上点击下方开始您的稳定高速上网体验.
+          <a href="auth/login.php" class="btn btn-lg btn-dark b-white bg-empty m-sm">登陆使用</a>
+          <a href="auth/register.php" class="btn btn-lg btn-warning b-white bg-empty m-sm">注册账户</a>
+        </div>
+      </div>
+      <i class="fa fa-caret-down fa-4x text-primary m-b-n-lg block"></i>
+	</div>
+  
+	<div class="bg-dark dker wrapper">
+		<div class="container text-center m-t-lg">
+			<div class="row m-t-xl m-b-xl">
+				<div class="col-sm-4" data-ride="animated" data-animation="fadeInLeft" data-delay="300">
+					<i class="fa fa-bullhorn fa-3x icon-muted"></i>
+					<h5 class="text-uc m-b m-t-lg">免责声明</h5>
+					<p class="text-sm">使用 <a href="index.php" title="Freedom">[Freedom]</a> 请遵守当地法律 <br>
+						Freedom不承担因使用本站服务所发生的任何责任.
+             	</p>
+				</div>
+				<div class="col-sm-4" data-ride="animated" data-animation="fadeInUp" data-delay="600">
+					<i class="fa fa-ticket fa-3x icon-muted"></i>
+					<h5 class="text-uc m-b m-t-lg">工单服务</h5>
+					<p class="text-sm"><a href="auth/login.php">在线工单技术支持,最快回复</a><br>在使用本站服务过程中有任何问题欢迎提交工单.</p>
+				</div>
+				<div class="col-sm-4" data-ride="animated" data-animation="fadeInRight" data-delay="900">
+					<i class="fa fa-desktop fa-3x icon-muted"></i>
+					<h5 class="text-uc m-b m-t-lg">远程支持</h5>
+					<p class="text-sm"></p><div class="follow-us">在线客服QQ： 528327016 <br>邮箱：528327016#qq.com(联系时请将#改为@)<p></p>
+				</div>
+			</div>
+			<br>
+			<div class="m-t-xl m-b-xl">
+				<p> 
+            <a href="tos.php#content" data-jump="true" class="btn btn-icon btn-rounded btn-dark b-dark bg-empty m-sm text-muted"><i class="fa fa-angle-up"></i></a>
+          	</p>
+			</div>
+			<div class="m-t-xl m-b-xl">
+				<p> 
+            Powered By<a href="staff" class="b-dark bg-empty m-sm text-muted">Shadowsocks & SS-panel-V2</a>
+          	</p>
+			</div>
+		</div>
+	</div>
+</footer>
+  <script src="theme/freedom/js/jquery.min.js"></script>
+  <script src="theme/freedom/js/bootstrap.js"></script>
+  <script src="theme/freedom/js/app.js"></script>
+  </body>
+</html>
